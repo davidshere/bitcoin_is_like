88cd27a48ee25c0c7b5fdaa663c9cbcab60c3009
davidshere <sheredlk@gmail.com>
2015-03-18 22:20:48 -0700

    Removed superfluous functions, now fetches 100 equity series (can fetch 2600)

--- fetcher.py
+++ fetcher.py
@@ -6,101 +6,58 @@ import requests
 import pandas as pd	
 import numpy as np
 
-import fred
 import Quandl as q
-from config import FRED_API_KEY
 
 
 COINBASE_ENDPOINT = 'https://www.coinbase.com/charts/price_history?days=365'
 COINBASE_ENDPOINT_VARIABLE_DAYS = 'https://www.coinbase.com/charts/price_history?days=%s'
 
 QUANDL_ENDPOINT = 'https://www.quandl.com/api/v1/datasets/%s/%s?sort_order=json.asc'
-QUANDL_CLOSING_PRICE_COLUMN_NUMBER = 10
+
 QUANDL_API_KEY = 'XL6P_9uvtZeqd6fFBQzB'
 
-
-#initializing fred with my api key
-fred.key(FRED_API_KEY)
-
-# FX values should be in the number of dollars per unit of foreign currency
-FRED_FX_NAMES = {
-	'fx_us_euro': 'dexuseu',
-	'fx_us_uk': 'dexusuk',
-}
-
-
-QUANDL_SERIES = {
-	'ECB': {
-		'Australian Dollar (AUD)':'EURAUD',
-		'Bulgarian Lev (BGN)':'EURBGN',
-		'Brazilian Real (BRL)':'EURBRL',
-		'Canadian Dollar (CAD)':'EURCAD',
-		'Swiss Franc (CHF)':'EURCHF',
-		'Chinese Yuan (CNY)':'EURCNY',
-		'Czech Koruna (CZK)':'EURCZK',
-		'Danish Krone (DKK)':'EURDKK',
-		'British Pound (GBP)':'EURGBP',
-		'Hong Kong Dollar (HKD)':'EURHKD',
-		'Croatian Kuna (HRK)':'EURHRK',
-		'Hungarian Forint (HUF)':'EURHUF',
-		'Indonesian Rupiah (IDR)':'EURIDR',
-		'New Israeli Shekel (ILS)':'EURILS',
-		'Indian Rupee (INR)':'EURINR',
-		'Icelandic Krona (ISK)':'EURISK',
-		'Japanese Yen (JPY)':'EURJPY',
-		'Korean Won (KRW)':'EURKRW',
-		'Lithuanian Lita (LTL)':'EURLTL',
-		'Mexican Peso (MXN)':'EURMXN',
-		'Malaysian Ringgit (MYR)':'EURMYR',
-		'Norwegian Krone (NOK)':'EURNOK',
-		'New Zealand Dollar (NZD)':'EURNZD',
-		'Philippine Peso (PHP)':'EURPHP',
-		'Polish Zloty (PLN)':'EURPLN',
-		'Romanian Leu (RON)':'EURRON',
-		'Russian Rubble (RUB)':'EURRUB',
-		'Swedish Krona (SEK)':'EURSEK',
-		'Singapore Dollar (SGD)':'EURSGD',
-		'Thai Baht (THB)':'EURTHB',
-		'Turkish Lira (TRY)':'EURTRY',
-		'US Dollar (USD)':'EURUSD',
-		'South African Rand (ZAR)':'EURZAR'
-	}
-}
-
-
-def fetch_quandl_series(series, kind):
-	if kind == 'fx': # make sure we're fetching the correct column
-		column = 1
-	if kind == 'stock':
-		column = 10
-
-	data = q.get(series.upper(),  # fetch ze column!
-				 trim_start='2010-03-05', 
-				 column=column,
-				 authtoken = QUANDL_API_KEY
-				)
-	name = series.split('/')[1]
-	if column == 10:
-		data.columns = pd.Index([name])
-	if column == 1:
-		data.columns = pd.Index([name])
-	return data
-
-
-STOCK_CODE_FILENAME = 'wiki_stock_codes.json'
+EQUITY_INFILE = 'wiki_stock_codes.json'
+EQUITY_OUTFILE = 'wiki_stock_series.json'
 
 class Fetcher(object):
+	""" 
+	A class to fetch data from the Quandl. 
+
+	 * V1 fetches sample data
+	 * V2 will interact with the DB to only fetch fresh data
+	"""
+
+	QUANDL_CLOSING_STOCK_PRICE_COLUMN_NUMBER = 10
+	QUANDL_CLOSING_FX_COLUMN_NUMBER = 1
 
 	def fetch_commodity_price_data(self):
 		pass
 
+
+	def _fetch_quandl_series(self, series, kind):
+		if kind == 'fx': # make sure we're fetching the correct column
+			column = self.QUANDL_CLOSING_STOCK_PRICE_COLUMN_NUMBER
+		if kind == 'stock':
+			column = self.QUANDL_CLOSING_FX_COLUMN_NUMBER
+		data = q.get(series.upper(),  # fetch ze column!
+					 trim_start='2010-03-05', 
+					 column=column,
+					 authtoken = QUANDL_API_KEY
+					)
+		name = series.split('/')[1]
+		if column == 10:
+			data.columns = pd.Index([name])
+		if column == 1:
+			data.columns = pd.Index([name])
+		return data
+
 	def fetch_stock_data(self):
 		first = True
-		codes = pd.read_json('wiki_stock_codes.json')
+		codes = pd.read_json(EQUITY_INFILE)
 		print codes.shape
 		for i, code in enumerate(codes.iloc[:,0]):
 			print "Fetching series %d of %d, %s" % (i, len(codes.index), code)
-			series = fetch_quandl_series(code, kind='stock')
+			series = self._fetch_quandl_series(code, kind='stock')
 			if first:
 				stock_frame = series
 				first = False
@@ -124,10 +81,7 @@ class Fetcher(object):
 def add_new_time_series_to_data_df(base_series, df):
 	#TODO: Should check how many columns are in the base series. If it's only one
 	#      make sure to call time_series_to_index on it
-	first_merge = False
-	if len(base_series.columns) == 1: # if we haven't joined any series together yet, we need to make sure to index the base_series
-		first_merge = True
-
+	first_merge = True
 	new_frame = base_series.join(df) # join two dfs together
 	series_name = df.dtypes.index[0] # get field name of non bitcoin series
 	# fill missing non bitcoin data
@@ -146,24 +100,10 @@ def add_new_time_series_to_data_df(base_series, df):
 def time_series_to_index(df, column_name):
 	return  df[column_name] / df[column_name].iloc[0]
 
-def fetch_all_ecb_quandl_data():
-		df = pd.DataFrame()
-		for series in QUANDL_SERIES['ECB']:
-			print series	
-			s = QUANDL_SERIES['ECB'][series]
-			param = '%s/%s' % ('ECB', s)
-			data = fetch_quandl_series(param, 'fx')
-			if df.size == 0:
-				df = data
-			else:
-				try:
-					df = add_new_time_series_to_data_df(df, data)
-				except:
-					print '%s failed join' % series
-		return df
-
 if __name__ == '__main__':
 	f = Fetcher()
+
+	# fetch and write stock data
 	data = f.fetch_stock_data()
-	print data.head()
+	data.to_json(EQUITY_OUTFILE)
 	
