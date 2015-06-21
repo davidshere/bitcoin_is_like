b93fc14522c7100c50ec6755d48eb60a3e9e6437
davidshere <sheredlk@gmail.com>
2015-06-20 18:03:14 -0700

    Add single ddl file to populate bitcoin row in dim_series

---
+++ ddl/add_bitcoin_data_to_dim_series.sql
@@ -0,0 +1,13 @@
+﻿insert into dim_series (
+	quandl_code,
+	source_code,
+	code,
+	series_name,
+	description)
+VALUES (
+	NULL,
+	'bitcoinaverage',
+	NULL,
+	'Bitcoin',
+	'Bitcoin is an implementation of a distributed record-keeping technology called the blockchain. It can be used to make financial transacations without the intermediation of a financial institution.'
+)
