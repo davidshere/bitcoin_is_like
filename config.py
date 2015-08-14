1b9f2c3b72257a3c2a8799d4c5456bc78a624892
davidshere <sheredlk@gmail.com>
2015-08-13 22:31:08 -0700

    Change config.py to pull from environmental variables instead of a YAML file

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+ENV = 'dev'
+
 CONFIG_VARIABLES = {
 	'database': 'postgres',
 	'username': os.environ['USERNAME'],
@@ -7,6 +9,11 @@ CONFIG_VARIABLES = {
 	'port': os.environ['DB_PORT'],
 	'hostname': os.environ['HOSTNAME']
 }
+
+if ENV == 'prod':
+	CONFIG_VARIABLES['username'] = 'dshere_master'
+	CONFIG_VARIABLES['hostname'] = 'postgresql-shere.cayimxhn4zl7.us-west-2.rds.amazonaws.com' 
+
 
 QUANDL_API_KEY = os.environ['QUANDL_KEY']
 SECRET_KEY = os.environ['FLASK_WTF_SECRET_KEY']
